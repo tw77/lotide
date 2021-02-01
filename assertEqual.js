978384f7938160f06409a2d34110e811f00bca32
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-01 23:58:38 +0000

    Making an assertion function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🚀🚀🚀 Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("🚫🚫🚫 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 0);
