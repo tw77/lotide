865e3cc9f146d995ccb0ef51c3d9a94efff02784
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-02 00:11:14 +0000

    Adding a function for retrieving the first element of an array

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🚀🚀🚀 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), undefined);
