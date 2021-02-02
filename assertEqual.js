69a8f6787697fd8ea88029eba8e32cc7489bb93b
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-02 00:04:14 +0000

    Improving function with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🚀🚀🚀 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🚀🚀🚀 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("🚫🚫🚫 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
