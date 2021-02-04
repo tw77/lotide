1f7d89e8612ceaf1b6472947ad90890ec1e55929
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-04 03:00:33 +0000

    Adding a function for returning the indices in a string where each letter is found

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = function(actualArray, expectedArray) {
+  let isTrue = 1;
+
+  for (let i = 0; i < actualArray.length; i++) {
+    if (actualArray[i] !== expectedArray[i]) {
+      isTrue -= 1;
+      console.log(`🚫🚫🚫 Assertion Failed: ${actualArray} !== ${expectedArray}`);
+    }
+  }
+
+  if (isTrue) {
+    console.log(`🚀🚀🚀 Assertion Passed: ${actualArray} === ${expectedArray}`);
+  }
+};
+
+
+
+// ACTUAL FUNCTION:
+
+const letterPositions = function(sentence) {
+  const results = {};
+  sentence = sentence.replace(/\s+/g, '');
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+
+// TEST CASES:
+assertArraysEqual(letterPositions("light house").h, [3, 5]);
+assertArraysEqual(letterPositions("lighthouse").i, [1]);
+
