d125f0f13d307cbcf6939b80bc9cd2d28a91fc57
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-05 01:04:00 +0000

    Adding a function to find keys by value, now involving a callback

---
+++ findKey.js
@@ -0,0 +1,50 @@
+const findKey = function(object, callback) {
+  let keyArray = Object.keys(object);
+
+  for (const key of keyArray) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+}
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🚀🚀🚀 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma")
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri")
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 2000 }
+}, x => x.stars === 2000), "Akelarre")
