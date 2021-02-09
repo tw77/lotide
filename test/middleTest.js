12b4ed40d5d9933917b117b5516d5257e5efc10f
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-09 03:27:07 +0000

    Implementing Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,11 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-
-assertArraysEqual((middle([1, 2, 5, 6, 7, 6, 3])), [6]);
-assertArraysEqual((middle([1, 2, 5, 7, 6, 3])), [5, 7]);
+describe("#middle", () => {
+  it("returns [6] for [1, 2, 5, 6, 7, 6, 3]", () => {
+    assert.deepEqual(middle([1, 2, 5, 6, 7, 6, 3]), [6]);
+  });
+  it("returns [5, 7] for [1, 2, 5, 7, 6, 3]", () => {
+    assert.deepEqual(middle([1, 2, 5, 7, 6, 3]), [5, 7]); 
+  });
+});
