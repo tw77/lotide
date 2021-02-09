e28a63f06706b3e6d605bf2c27113435ba5216e1
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-09 04:15:46 +0000

    Refactored eqArrays function to account for previously overlooked potential cases

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,14 @@
 const eqArrays = function(array1, array2) {
-
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  for (let i = array2.length -1; i > -1; i--) {
+    if (array2[i] !== array1[i]) {
       return false;
     }
   }
@@ -9,3 +16,4 @@ const eqArrays = function(array1, array2) {
 };
 
 module.exports = eqArrays;
+
