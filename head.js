014464badd5d7f86fc3a0984058eda219a2e936d
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-09 00:29:35 +0000

    Refactoring with modules

--- head.js
+++ head.js
@@ -1,16 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🚀🚀🚀 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+
+module.exports = head;
