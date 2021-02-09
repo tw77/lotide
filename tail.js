65fba4a6de57d6267166a5df20b44e37fdb80850
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-09 00:45:38 +0000

    Refactoring with modules

--- tail.js
+++ tail.js
@@ -1,19 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🚀🚀🚀 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return array.splice(1);
 };
 
-let words = ["Yo Yo", "Lighthouse", "Labs"];
 
-const tailedWords = tail(words);
-
-assertEqual(tailedWords[0], "Lighthouse");
-assertEqual(tailedWords.length, 2);
-assertEqual(words[0], "Yo Yo");
+module.exports = tail;
