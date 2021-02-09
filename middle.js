80f12d85ecb804df287b7f63df55795db1a70185
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-09 02:15:23 +0000

    Refactoring with modules

--- middle.js
+++ middle.js
@@ -1,31 +1,3 @@
-const eqArrays = function(array1, array2) {
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actualArray, expectedArray) {
-  let isTrue = 1;
-
-  for (let i = 0; i < actualArray.length; i++) {
-    if (actualArray[i] !== expectedArray[i]) {
-      isTrue -= 1;
-      console.log(`🚫🚫🚫 Assertion Failed: ${actualArray} !== ${expectedArray}`);
-    }
-  }
-
-  if (isTrue) {
-    console.log(`🚀🚀🚀 Assertion Passed: ${actualArray} === ${expectedArray}`);
-  }
-};
-
-
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   let outputArray = [];
   let middleIndex = 0;
@@ -46,6 +18,4 @@ const middle = function(array) {
   return outputArray;
 }
 
-// TEST CODE
-assertArraysEqual((middle([1, 2, 5, 6, 7, 6, 3])), [6]);
-assertArraysEqual((middle([1, 2, 5, 7, 6, 3])), [5, 7]);
+module.exports = middle;
